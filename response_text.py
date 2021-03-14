3f73e3b265cd9be3b3aa91dadf970897c6727fb6
feagans <70381848+feagans@users.noreply.github.com>
2021-03-13 22:16:43 -0500

    Update response_text.py

--- response_text.py
+++ response_text.py
@@ -1,18 +1,3 @@
-from flask import Flask, request, redirect
-from twilio.twiml.messaging_response import MessagingResponse
-
-app = Flask(__name__)
-
-@app.route("/sms", methods=['GET', 'POST'])
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Start our TwiML response
-    resp = MessagingResponse()
-
-    # Add a message
-    resp.message("The Robots are coming! Head for the hills!")
-
-    return str(resp)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+<Response>
+	<Message>Thanks for the message. Configure your number's SMS URL to change this message.Reply HELP for help.Reply STOP to unsubscribe.Msg&Data rates may apply.</Message>
+</Response>
